cbf881bdad28b37bd7cb419cd22c03a35033012a
Евдокимов Виталий <114080800+FoNoMaster@users.noreply.github.com>
2023-03-09 21:09:10 +0300

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -4,6 +4,13 @@
 #include <fstream>
 #include <string>
 
+void swap(int* a, int* b, int &c){
+    int t = *a;
+    *a = *b;
+    *b = t;
+    c++;
+}
+
 int rand_uns(int min, int max) {
     unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
     static std::default_random_engine e(seed);
@@ -12,12 +19,14 @@ int rand_uns(int min, int max) {
 }
 
 int main() {
-    int a[100], file_number = 1;
-    for (int l = 0;l < 100; l++){
+    int a[100];
+    int file_number = 0;
+    int* FN_link = &file_number;
+    for (int l = 0; l < 100; l++){
         a[l] = l + 1;
     }
     for (int k = 0; k < 1000; k++){
-        int r = rand_uns(1, 99), t = rand_uns(1, 99);
+        int r = rand_uns(0, 99), t = rand_uns(0, 99);
         int tmp = a[r];
         a[r] = a[t];
         a[t] = tmp;
@@ -25,15 +34,16 @@ int main() {
     for (int i = 0; i < 100; i++)
         for (int j = 0; j < 100-i-1; j++)
             if (a[j] > a[j+1]){
-                int tmp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = tmp;
+                swap(&a[j], &a[j+1], *FN_link);
 
-                std::ofstream file( std::string( "sort_bubble" + std::to_string( file_number ) + ".txt").c_str() );
+                std::ofstream file( std::string( "C:\\Users\\12345\\CLionProjects\\huy\\sort_bubble" + std::to_string( file_number ) + ".txt").c_str() );
                 for (int h = 0; h < 100; h++){
                     file << a[h] << std::endl;
                 }
                 file.close();
-                file_number += 1;
             }
+//    for (int i = 0; i < 100; i++){
+//        std::cout << a[i] << std::endl;
+//    }
+//    std::cout << file_number;
 }

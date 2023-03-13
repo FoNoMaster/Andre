94ccfa68acaa41fbb71d2a87d64fe927f13a41d9
kharanzhevskii <114580990+kharanzhevskii@users.noreply.github.com>
2023-03-13 13:08:29 +0300

    Update png_to_gif.py

--- png_to_gif.py
+++ png_to_gif.py
@@ -1,15 +1,21 @@
 from PIL import Image
 import glob
- 
+
+
+def sort_func(x):
+    return int(x[11:-4])
+
 # Create the frames
 frames = []
 imgs = glob.glob("*.png")
+imgs.sort(key=sort_func)
 for i in imgs:
     new_frame = Image.open(i)
     frames.append(new_frame)
  
 # Save into a GIF file that loops forever
-frames[0].save('andre.gif', format='GIF',
-               append_images=frames[1:],
-               save_all=True,
-               duration=5000, loop=1)
+gif_name = "0" + imgs[0][:-5] + ".gif"
+frames[0].save(gif_name, format='GIF',
+                append_images=frames[1:],
+                save_all=True,
+                duration=20, loop=0)

aaac6a2f5d60255e627120cdc7cfbf26e8a0b2e6
kharanzhevskii <114580990+kharanzhevskii@users.noreply.github.com>
2023-03-13 13:07:13 +0300

    andre loh

--- all_txt_to_png.py
+++ all_txt_to_png.py
@@ -6,12 +6,54 @@ import numpy as np
 files = glob.glob("*.txt")
 for i in files:
     new_file = np.loadtxt(i, dtype=int)
-    w, h = 960, 540
-    img = Image.new("RGB", (w, h))
-    img1 = ImageDraw.Draw(img)
-    for j in range(100):
-        shape = [(31+9*j, 520),(38+9*j, 520-new_file[j]*5)]
-        img1.rectangle(shape, fill="#ffff33", outline="red")
-    image_name = i[:-4] + ".png"
-    img.save(image_name)
+    size = len(new_file)
+    if size == 100:
+        w, h = 960, 540
+        img = Image.new("RGB", (w, h))
+        img1 = ImageDraw.Draw(img)
+        for j in range(100):
+            shape = [(31+9*j, 520),(38+9*j, 520-new_file[j]*5)]
+            img1.rectangle(shape, fill="#ffff33", outline="red")
+        image_name = i[:-4] + ".png"
+        img.save(image_name)
+    elif size == 256:
+        w, h = 1920, 1080
+        img = Image.new("RGB", (w, h))
+        img1 = ImageDraw.Draw(img)
+        for j in range(256):
+            shape = [(64+7*j, 1052),(69+7*j, 1052-new_file[j]*4)]
+            img1.rectangle(shape, fill="#ffff33", outline="red")
+        image_name = i[:-4] + ".png"
+        img.save(image_name)
+
+the_file = files[0][:-5] + "10000.txt"
+test_file = [x+1 for x in range(100)]
+size = len(test_file)
+if size == 100:
+    for i in range(100):
+        w, h = 960, 540
+        img = Image.new("RGB", (w, h))
+        img1 = ImageDraw.Draw(img)
+        for j in range(100):
+            shape = [(31+9*j, 520),(38+9*j, 520-test_file[j]*5)]
+            if j > i:
+                img1.rectangle(shape, fill="#ffff33", outline="red")
+            else:
+                img1.rectangle(shape, fill="red", outline="red")
+        image_name = the_file[:11] + str(int(the_file[11:-4])+1+i) + ".png"
+        img.save(image_name)
+if size == 256:
+    for i in range(256):
+        w, h = 1920, 1080
+        img = Image.new("RGB", (w, h))
+        img1 = ImageDraw.Draw(img)
+        for j in range(256):
+            shape = shape = [(64+7*j, 1052),(69+7*j, 1052-test_file[j]*4)]
+            if j > i:
+                img1.rectangle(shape, fill="#ffff33", outline="red")
+            else:
+                img1.rectangle(shape, fill="red", outline="red")
+        image_name = the_file[:11] + str(int(the_file[11:-4])+1+i) + ".png"
+        img.save(image_name)
+
 
